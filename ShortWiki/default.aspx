37702d4476c95c3fb32219c07c56e2e73ec4281f
Abdulla Karimov <akarimov@accesssoftek.com>
2019-11-08 01:52:56 +0300

    init

---
+++ ShortWiki/default.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" CodeFile="default.aspx.cs" Inherits="ShortWiki._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Wikipedia shortened</title>
+    <link href='http://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'/>
+    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,300' rel='stylesheet' type='text/css'/>
+    <link href="styles/style.css" rel="stylesheet" type="text/css"/>
+    <%--<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/javascript"></script>--%>
+    <meta name="viewport" content="width=device-width, maximum-scale=1.0, initial-scale=1.0, user-scalable=0"/>
+    <!-- Created by @getlaidanddie -->
+</head>
+<body>
+    <div class="wrapper">
+        <form id="form1" runat="server">
+        <h1>
+            <asp:ScriptManager ID="ToolkitScriptManager1" runat="server">
+
+            </asp:ScriptManager>
+            
+            Shorter Wikipedia
+        </h1>
+        <div class="content">
+            <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+                <ContentTemplate>
+                    <span class="query">
+                        <div class="queryWrap">
+                        <asp:TextBox ID="searchQuery" runat="server"></asp:TextBox>
+                        <ajaxToolkit:AutoCompleteExtender runat="server" ID="AutoComplete1" ServiceMethod="GetCompletionList"
+                             TargetControlID="searchQuery" UseContextKey="True" Enabled="True" CompletionSetCount="3"
+                             CompletionInterval="100" EnableCaching="True" MinimumPrefixLength="1"
+                             CompletionListCssClass="searchCompletionList" CompletionListItemCssClass="searchCompletionItem" CompletionListHighlightedItemCssClass="searchCompletionHighlight"/>
+                        <asp:Button ID="searchButton" runat="server" Text="Search" OnClick="searchButton_Click" />
+                        </div>
+                    </span>
+                    <asp:UpdateProgress ID="UpdateProgress1" runat="server">
+                        <ProgressTemplate>
+                            <img src="images/loading.gif" class="loading"/>
+                        </ProgressTemplate>
+                    </asp:UpdateProgress>
+                    <div class="result"><asp:Label ID="searchResult" runat="server"></asp:Label></div>
+                </ContentTemplate>
+                <Triggers>
+                    <asp:AsyncPostBackTrigger ControlID="aboutLink"/>
+                </Triggers>
+            </asp:UpdatePanel>
+            <span class="respect">From <a href="https://en.wikipedia.org/">Wikipedia</a>, the free encyclopedia | <asp:LinkButton ID="aboutLink" runat="server" OnClick="aboutLink_Click">What is this?</asp:LinkButton></span>
+    
+        </div>
+        </form>
+    </div>
+</body>
+    <script>
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date(); a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', 'UA-55214263-1', 'auto');
+        ga('send', 'pageview');
+
+</script>
+</html>
---
+++ ShortWiki/default.aspx.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.Linq;
+using Humanizer;
+
+namespace ShortWiki
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        public string enWpApiUrl = "https://en.wikipedia.org/w/api.php?action=query&prop=categories&cllimit=50&format=xml&clshow=!hidden&redirects&titles=";
+        public string wdApiUrl = "https://www.wikidata.org/w/api.php?action=wbgetentities&sites=enwiki&format=xml&props=descriptions&titles=";
+        public string enWpOpenSearchUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&limit=1&namespace=0&format=xml&search=";
+        public string resultString = "";
+        private string resultText = "";
+
+        public string[] nonPlural =
+        {
+            "california",
+            "russia",
+            "texas",
+            "illinois",
+            "malus",
+            "travis",
+            "los",
+            "os"
+        };
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public string getOriginalTitle(string title)
+        {
+            //get original page title
+            string nameNodePath = "/api/query/redirects/r";
+            string url = enWpApiUrl + title;
+            XmlDocument resultXml = new XmlDocument();
+            resultXml.Load(url);
+            XmlElement root = resultXml.DocumentElement;
+            XmlNode nameNode = root.SelectSingleNode(nameNodePath);
+            string originalName = title;
+            if (nameNode != null) originalName = nameNode.Attributes["to"].Value;
+            return originalName;
+        }
+
+        public string getCategories(string title)
+        {
+            //get category titles from EnWiki and convert them to singularized text
+            string name = title.Replace(" ", "_");
+            string categoryNodePath = "/api/query/pages/page/categories/cl";
+            string result = "";
+            string url = enWpApiUrl + name;
+            XmlDocument resultXml = new XmlDocument();
+            resultXml.Load(url);
+            XmlElement root = resultXml.DocumentElement;
+            string originalTitle = getOriginalTitle(name);
+            string originalName = originalTitle.Replace(" ", "_");
+
+            XmlNodeList categoryNodeList = root.SelectNodes(categoryNodePath);
+            foreach (XmlNode categoryNode in categoryNodeList)
+            {
+                string categoryName = categoryNode.Attributes["title"].Value;
+                categoryName = categoryName.Replace("Category:", "");
+                if (Has(categoryName, "article") || Has(categoryName, "page") || Has(categoryName, "error") ||
+                    Has(categoryName, "stub") || categoryName.ToLower() == name.ToLower() ||
+                    categoryName.ToLower() == originalName.ToLower() ||
+                    categoryName.ToLower().Singularize() == title.ToLower() ||
+                    categoryName.ToLower().Singularize() == originalName.ToLower()) categoryName = null;
+
+                if (!String.IsNullOrEmpty(categoryName))
+                {
+                    if (categoryName.Contains(' '))
+                    {
+                        foreach (string word in categoryName.Split(' '))
+                        {
+                            if (!nonPlural.Contains(word.ToLower()))
+                            {
+                                categoryName = categoryName.Replace(word, word.Singularize(false));
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (!nonPlural.Contains(categoryName.ToLower())) categoryName = categoryName.Singularize(false);
+                    }
+                    result += categoryName + ".<br />";
+                }
+            }
+            return result;
+        }
+
+        public XmlElement getOpenSearchResultRoot(string query)
+        {
+            enWpOpenSearchUrl += query;
+            XmlDocument tempXml = new XmlDocument();
+            tempXml.Load(enWpOpenSearchUrl);
+            string osXmlString = tempXml.OuterXml;
+            string filter = @"xmlns(:\w+)?=""([^""]+)""|xsi(:\w+)?=""([^""]+)""";
+            osXmlString = Regex.Replace(osXmlString, filter, "");
+            XmlDocument openSearchXmlDoc = new XmlDocument();
+            openSearchXmlDoc.LoadXml(osXmlString);
+            XmlElement root = openSearchXmlDoc.DocumentElement;
+            return root;
+        }
+
+        public XmlDocument getOpenSearchResultDoc(string query)
+        {
+            enWpOpenSearchUrl += query;
+            XmlDocument tempXml = new XmlDocument();
+            tempXml.Load(enWpOpenSearchUrl);
+            string osXmlString = tempXml.OuterXml;
+            string filter = @"xmlns(:\w+)?=""([^""]+)""|xsi(:\w+)?=""([^""]+)""";
+            osXmlString = Regex.Replace(osXmlString, filter, "");
+            XmlDocument openSearchXmlDoc = new XmlDocument();
+            openSearchXmlDoc.LoadXml(osXmlString);
+            return openSearchXmlDoc;
+        }
+
+        public string getOpenSearchResultPageTitle(string query)
+        {
+            string pageTitle = "";
+            XmlElement root = getOpenSearchResultRoot(query);
+            XmlNode pageTitleNode = root.SelectSingleNode("//Text");
+            if (pageTitleNode != null) pageTitle = pageTitleNode.InnerText;
+            return pageTitle;
+        }
+
+        public string getOpenSearchResultPageBody(string query)
+        {
+            string pageBody = "";
+            //XmlElement root = getOpenSearchResultRoot(query);
+            XmlDocument doc = getOpenSearchResultDoc(query);
+            XmlNode pageBodyNode = doc.SelectSingleNode("//Description");
+            if (pageBodyNode != null) pageBody = pageBodyNode.InnerText;
+            return Regex.Replace(pageBody, @" ?\(.*?\)", string.Empty);
+        }
+
+        public string getOpenSearchResultImage(string query)
+        {
+            string image = "";
+            XmlElement openSearchXmlRoot = getOpenSearchResultRoot(query);
+            XmlNode imageNode = openSearchXmlRoot.SelectSingleNode("/SearchSuggestion/Section/Item/Image");
+            if (imageNode != null) image = imageNode.Attributes["source"].Value;
+            return image;
+        }
+
+        public string getOpenSearchResultPageUrl(string query)
+        {
+            string pageUrl = "";
+            XmlElement root = getOpenSearchResultRoot(query);
+            XmlNode pageUrlNode = root.SelectSingleNode("//Url");
+            if (pageUrlNode != null) pageUrl = pageUrlNode.InnerText;
+            return pageUrl;
+        }
+
+        public static XmlDocument RemoveXmlns(XmlDocument doc)
+        {
+            XDocument d;
+            using (var nodeReader = new XmlNodeReader(doc))
+                d = XDocument.Load(nodeReader);
+
+            d.Root.Descendants().Attributes().Where(x => x.IsNamespaceDeclaration).Remove();
+
+            foreach (var elem in d.Descendants())
+                elem.Name = elem.Name.LocalName;
+
+            var xmlDocument = new XmlDocument();
+            using (var xmlReader = d.CreateReader())
+                xmlDocument.Load(xmlReader);
+
+            return xmlDocument;
+        }
+        
+        protected void searchButton_Click(object sender, EventArgs e)
+        {
+            string name = searchQuery.Text;
+            ShowResults(name);
+        }
+
+        public void ShowResults(string name)
+        {
+            if (!String.IsNullOrEmpty(name))
+            {
+                if (name.Length > 200)
+                {
+                    resultText = "Query string is too long.";
+                }
+                else
+                {
+                    string resultString = getCategories(name);
+                    //get description from Wikidata
+                    string originalTitle = getOriginalTitle(name);
+                    string originalName = originalTitle.Replace(" ", "_");
+                    wdApiUrl += originalName;
+                    XmlDocument wikidataXML = new XmlDocument();
+                    wikidataXML.Load(wdApiUrl);
+                    XmlElement wdRoot = wikidataXML.DocumentElement;
+                    XmlNode wdDescNode = wikidataXML.SelectSingleNode("/api/entities/entity/descriptions/description[@language='en']");
+                    string wdDesc = "";
+                    if (wdDescNode != null) wdDesc = wdDescNode.Attributes["value"].Value;
+                    string wikiUrl = "https://en.wikipedia.org/wiki/" + originalName;
+                    string openSearchUrl = getOpenSearchResultPageUrl(name);
+
+                    string wikiLink = "<b><a href='https://en.wikipedia.org/wiki/" + originalName.Replace("'", "%27") + "'>" + originalTitle +
+                                      "</a></b>";
+                    string openSearchDesc = getOpenSearchResultPageBody(name);
+                    string openSearchTitle = getOpenSearchResultPageTitle(name);
+
+                    if (!String.IsNullOrEmpty(wdDesc))
+                    {
+                        resultText += wikiLink + " is " + wdDesc + ".<br />";
+                    }
+                    if (!String.IsNullOrEmpty(openSearchDesc) && String.IsNullOrEmpty(wdDesc) && openSearchUrl==wikiUrl)
+                    {
+                        openSearchDesc.Replace(originalTitle, wikiLink);
+                        resultText += openSearchDesc + "<br />";
+                    }
+                    if (!String.IsNullOrEmpty(resultString))
+                    {
+                        resultText += "These facts are known about " + wikiLink + ":<br />" + resultString;
+                    }
+                    if (String.IsNullOrEmpty(wdDesc) && String.IsNullOrEmpty(resultString))
+                    {
+                        resultText = "Sorry, not enough information about " + wikiLink + " to display.";
+                    }
+                    searchResult.Text = resultText;
+                }
+            }
+        }
+
+        public bool Has(string c, string str)
+        {
+            return c.ToLower().Contains(str);
+        }
+
+        protected void aboutLink_Click(object sender, EventArgs e)
+        {
+            //displays the about text
+            const string aboutText = @"<i>Shorter Wikipedia</i> is a website aimed to get short, concise material from 
+                                <a href='https://en.wikipedia.org/wiki/Wikipedia'>Wikipedia, the free encyclopedia</a>.<br/>
+                                This service uses API of Wikipedia and <a href='https://en.wikipedia.org/wiki/Wikidata'>Wikidata</a>.<br />
+                                Singularization of category titles is done by <a href='https://humanizr.net/'>Humanizer</a>.";
+            searchResult.Text = aboutText;
+        }
+
+        [System.Web.Services.WebMethodAttribute(), System.Web.Script.Services.ScriptMethodAttribute()]
+        public static string[] GetCompletionList(string prefixText, int count, string contextKey)
+        {
+            string enWpApiStartsWithUrl = "https://en.wikipedia.org/w/api.php?action=query&generator=allpages&gaplimit=" + count + "&format=xml&gapfrom=";
+            List<string> resultList = new List<string>();
+            string prefix = prefixText;
+            if (!String.IsNullOrEmpty(prefix))
+            {
+                enWpApiStartsWithUrl += prefix.Replace(" ","_");
+                XmlDocument resultXML = new XmlDocument();
+                resultXML.Load(enWpApiStartsWithUrl);
+                XmlElement root = resultXML.DocumentElement;
+                XmlNodeList pages = resultXML.SelectNodes("/api/query/pages/page");
+                foreach (XmlNode page in pages)
+                {
+                    string title = page.Attributes["title"].Value;
+                    if(!String.IsNullOrEmpty(title)) resultList.Add(title);
+                }
+            }
+            string[] result = resultList.ToArray();
+            return result;
+        }
+    }
+}
